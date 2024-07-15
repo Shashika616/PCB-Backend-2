7abbbc8799239faebecf79e222b758c20a087f0b
Shashika616 <shashikauda@gmail.com>
2024-07-15 14:59:12 +0530

    new cors config file 1.1

--- src/main/java/com/exampleone/pcb/CorsConfig.java
+++ src/main/java/com/exampleone/pcb/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")  // Apply to all endpoints
-                        .allowedOrigins("*")  // Allow all origins, replace with specific origins if needed
+                        .allowedOrigins("http://localhost:3000", "https://fe1b9d98-4f90-40c6-a52e-8e2d1396f647.e1-us-east-azure.choreoapps.dev")  // Allow all origins, replace with specific origins if needed
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific methods
                         .allowedHeaders("*")  // Allow all headers
                         .allowCredentials(true)  // Allow credentials
